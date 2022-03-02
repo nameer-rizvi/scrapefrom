d7c6d8fd77b91a57bc204854639abbe6ffd671d2
nameer-rizvi <rizvinameer@gmail.com>
2022-03-02 09:13:52 -0500

    clear sandbox

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,33 +1,15 @@
 const scrapefrom = require("./index");
 
-const unionstageScraper = ({ venue = "UNION STAGE" }) => ({
-  use: "puppeteer",
-  logFetch: true,
-  timeout: 10000,
-  url: "https://www.unionstage.com/",
-  waitForSelector: "select.venue-select",
-  selectDropdown: ["select.venue-select", venue],
+scrapefrom({
+  url: "",
   extract: {
-    selector: "div[class='tessera-show-card']", // we need the exact class match because they add an extra class on hidden divs
-    name: "events",
-    extracts: [
-      { name: "date", selector: "div.tessera-date" },
-      { name: "time", selector: "span.tessera-showTimes" },
-      { name: "title", selector: "h4" },
-      { name: "ageRestriction", selector: "span.tessera-showAges" },
-      { name: "description", selector: "div.tessera-additionalArtists" },
-      { name: "eventLink", selector: "a.more-info", attribute: "href" },
-      { name: "location", select: "span.tessera-venue" },
-    ],
+    selector: "",
+    name: "",
+    extracts: [],
   },
-});
-
-scrapefrom([
-  unionstageScraper({ venue: "CAPITAL TURNAROUND" }),
-  unionstageScraper({ venue: "MIRACLE THEATRE" }),
-  unionstageScraper({ venue: "UNION STAGE" }),
-])
-  .then((responses) =>
-    responses.forEach((response) => console.log(response.result.events))
+})
+  .then(
+    (responses) => console.log(responses.result.events)
+    // responses.forEach((response) => console.log(response.result.events))
   )
   .catch(console.log);
